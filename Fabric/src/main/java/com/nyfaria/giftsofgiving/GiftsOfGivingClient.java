5cdc71c34c92cf3a1025a19d667d6684336157ab
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-12-03 18:22:15 -0500

    Add cutout for presents in Fabric

--- Fabric/src/main/java/com/nyfaria/giftsofgiving/GiftsOfGivingClient.java
+++ Fabric/src/main/java/com/nyfaria/giftsofgiving/GiftsOfGivingClient.java
@@ -3,8 +3,12 @@ package com.nyfaria.giftsofgiving;
 import com.nyfaria.giftsofgiving.client.CommonClientClass;
 import com.nyfaria.giftsofgiving.client.renderer.PresentBlockEntityRenderer;
 import com.nyfaria.giftsofgiving.init.BlockInit;
+import com.nyfaria.nyfsmultiloader.registration.RegistryObject;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
+import net.minecraft.world.level.block.Block;
 
 public class GiftsOfGivingClient implements ClientModInitializer {
     
@@ -12,5 +16,9 @@ public class GiftsOfGivingClient implements ClientModInitializer {
     public void onInitializeClient() {
         CommonClientClass.loadClass();
         BlockEntityRenderers.register(BlockInit.PRESENT_BLOCK_ENTITY.get(), PresentBlockEntityRenderer::new);
+
+        for (RegistryObject<Block> present : BlockInit.BLOCKS.getEntries()) {
+            BlockRenderLayerMap.INSTANCE.putBlock(present.get(), RenderType.cutout());
+        }
     }
 }
